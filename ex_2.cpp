c02b2dfe69732ebda07a710818015886ad837ccb
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-09-08 22:26:17 +0300

    start

---
+++ ex_2.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ex_2
+//
+//  Created by Danial on 08.09.2018.
+//  Copyright © 2018 Danial. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
