b7926482400fe6c5f8690bdceba45a0c8f5a64a6
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-09-08 22:04:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Danial on 06.09.2018.
+//  Copyright © 2018 Danial. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main( ) {
+    
+    for (int i =0 ; i < 5 ; i++)
+    {
+        cout << i+1 << " " <<endl;
+    }
+    return 0;
+}
