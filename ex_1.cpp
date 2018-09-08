1a85c5eb2c9223a1d55afd726d56de3ab437f9da
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-09-08 22:22:15 +0300

    5

--- ex_1.cpp
+++ ex_1.cpp
@@ -12,12 +12,16 @@ using namespace std;
 
 int main() {
     
+    int tmp = 1;
+    
     for (int i = 0 ; i < 10 ; i++)
     {
         for ( int j = 0; j < 10; j++)
         {
-            
+            cout << tmp << " ";
+            tmp ++;
         }
+        cout << endl;
     }
     return 0;
 }
