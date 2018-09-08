0a511f502ec8021afe2a93de22b31c6740957e67
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-09-08 22:17:43 +0300

    Add files via upload

--- ex_1.cpp
+++ ex_1.cpp
@@ -12,9 +12,12 @@ using namespace std;
 
 int main() {
     
-    for (int i =0 ; i < 10 ; i++)
+    for (int i = 0 ; i < 10 ; i++)
     {
-        
+        for ( int j = 0; j < 10; j++)
+        {
+            
+        }
     }
     return 0;
 }
