c07b5fdf9543e9127ee93db24d783c4f78761860
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-09-09 20:10:51 +0300

    working version

--- task5.cpp
+++ task5.cpp
@@ -10,15 +10,13 @@
 
 using namespace std;
 
-
-
 struct Node
 {
     Node* next;
     int data;
 };
 
-Node* create_list(const int data)
+Node* create_list(const int data = 0)
 {
     Node* root = new Node;
     root->next = NULL;
@@ -26,7 +24,7 @@ Node* create_list(const int data)
     return root;
 }
 
-Node* add_element (Node* root, const int data, const int position)
+Node* find_by_position (Node* root,  const int position)
 {
     if ( root == NULL )
     {
@@ -47,6 +45,18 @@ Node* add_element (Node* root, const int data, const int position)
         cerr << " wrong position " << endl;
         return NULL;
     }
+    
+    return root;
+}
+
+Node* add_element (Node* root, const int data, const int position)
+{
+    if ((root = find_by_position(root,position)) == NULL)
+    {
+        cerr << " find by position failed " << endl;
+        return NULL;
+    }
+    
     Node* add = new Node;
     add->data = data;
     add->next = root->next;
@@ -55,6 +65,17 @@ Node* add_element (Node* root, const int data, const int position)
     
     return add;
 }
+
+void erase_element (Node* root, const int position)
+{
+    if ((root = find_by_position(root,position)) == NULL)
+    {
+        cerr << " root element is NULL " << endl;
+    }
+    
+    root->next = root->next->next;
+}
+
 
 void show_list(Node* root)
 {
@@ -76,19 +97,99 @@ void show_list(Node* root)
     cout<<endl;
 }
 
+void push_element ( Node* root, const int data)
+{
+    root->data = data;
+    Node* add = new Node;
+    root->next = add;
+    add = NULL;
+}
+
+void add ( Node* root, const int data)
+{
+    while ( root->next)
+    {
+        root = root->next;
+    }
+    
+    Node* add = new Node;
+    root->next = add;
+    add->data = data;
+    add->next = NULL;
+}
+
+
+void split ( Node* root, Node* first, Node* second, const int N )
+{
+    if ( root == NULL )
+    {
+        cerr << " root element is NULL " << endl;
+        return;
+    }
+
+    bool tmp1 = false;
+    bool tmp2 = false;
+    
+    while(root)
+    {
+        if (root->data > N)
+        {
+            if (!tmp1)
+            {
+                first->data = root->data;
+                tmp1 = true;
+            }
+            else
+            {
+                add(first, root->data);
+                first = first->next;
+            }
+            
+        }
+        else
+        {
+            if (!tmp2)
+            {
+                second->data = root->data;
+                tmp2 = true;
+            }
+            else
+            {
+                add(second, root->data);
+                second = second->next;
+            }
+            
+        }
+    
+        root = root->next;
+    }
+}
+
 
 
 int main()
 {
-    Node* root;
-    
-    root = create_list(1);
+    Node* root = create_list(1);
+    Node* first = create_list();
+    Node* second = create_list();
     
     add_element(root, 2, 2);
     add_element(root, 4, 3);
     add_element(root, 5, 4);
     add_element(root, 3, 3);
     show_list(root);
+    //erase_element(root, 3);
+    // show_list(root);
+    
+    cout << endl << endl;
+    
+    split(root, first, second, 5);
+    
+    cout << " First list : ";
+    show_list(first);
+    cout << " Second list : ";
+    show_list(second);
+    
     
     return 0;
 }
