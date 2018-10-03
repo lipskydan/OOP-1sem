21ddbc33f73ef54ed3080063f7458663c9b6b52d
lipskydan <43038176+lipskydan@users.noreply.github.com>
2018-10-03 19:14:46 +0300

    lab1 can't compile

---
+++ labs/lab1/graph.hpp
@@ -0,0 +1,35 @@
+//
+//  graph.hpp
+//  lab1
+//
+//  Created by Danial on 03.10.2018.
+//  Copyright © 2018 Danial. All rights reserved.
+//
+
+#ifndef graph_hpp
+#define graph_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+template<class T> class Graf
+{
+public:
+    Graf(int size);
+    void initializeGraf();
+    void printAsMatrix();
+    void printAsList();
+    bool isLinked();
+private:
+    int mData;
+    vector<set<int>> mList;
+    vector<int> vec1;
+    vector<int> vec2;
+    vector<int> vec_full;
+};
+
+#endif /* graph_hpp */
---
+++ labs/lab1/lab1.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Danial on 03.10.2018.
+//  Copyright © 2018 Danial. All rights reserved.
+//
+
+#include <iostream>
+#include "graph.hpp"
+
+using namespace std;
+
+int main()
+{
+    int Size = 5;
+    Graf<int> first(Size);
+    first.initializeGraf();
+    first.printAsList();
+    first.printAsMatrix();
+    //first.isLinked();
+    
+    return 0;
+}
